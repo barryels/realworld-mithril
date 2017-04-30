8060b94335f2e676e77b850dbf259ea27ad3c53e
Barry Els <barryels@gmail.com>
2017-05-01 00:02:18 +0200

    one less <div>

--- src/ui/components/ScreenUserSettings.js
+++ src/ui/components/ScreenUserSettings.js
@@ -13,19 +13,15 @@ function oninit() {
 
 
 function view() {
-	return m('div',
-		[
-			m('.container.page', [
-				m('.row', [
-					m('.col-md-6.offset-md-3.col-xs-12', [
-						m('h1.text-xs-center', 'Your Settings'),
-						m(ListErrors, { errors: domain.store.userUpdateSettingsErrors }),
-						m(UserSettingsForm, { currentUser: domain.store.user, isUserSettingsUpdateBusy: domain.store.isUserSettingsUpdateBusy, fn_updateUserSettings: domain.actions.updateUserSettings, fn_logUserOut: domain.actions.logUserOut })
-					])
-				])
+	return m('.container.page', [
+		m('.row', [
+			m('.col-md-6.offset-md-3.col-xs-12', [
+				m('h1.text-xs-center', 'Your Settings'),
+				m(ListErrors, { errors: domain.store.userUpdateSettingsErrors }),
+				m(UserSettingsForm, { currentUser: domain.store.user, isUserSettingsUpdateBusy: domain.store.isUserSettingsUpdateBusy, fn_updateUserSettings: domain.actions.updateUserSettings, fn_logUserOut: domain.actions.logUserOut })
 			])
-		]
-	);
+		])
+	]);
 };
 
 
