583c322442a45f74fcdba87d412551cf551ee053
Barry Els <barryels@gmail.com>
2017-04-30 23:42:34 +0200

    renaming 'New Post' to 'New Article', inline with angularjs impl. advances #8

--- src/ui/components/MainNav.js
+++ src/ui/components/MainNav.js
@@ -19,7 +19,7 @@ function view(vnode) {
 
 	return m('ul', { className: vnode.attrs.className }, [
 		linkItemHome,
-		m('li.nav-item', m(Link, { className: 'nav-link', to: '/editor' }, [m('i.ion-compose'), m('span', ' New Post')])),
+		m('li.nav-item', m(Link, { className: 'nav-link', to: '/editor' }, [m('i.ion-compose'), m('span', ' New Article')])),
 		m('li.nav-item', m(Link, { className: 'nav-link', to: '/settings' }, [m('i.ion-gear-a'), m('span', ' Settings')])),
 		m('li.nav-item', m(Link, { className: 'nav-link', to: '/@' + currentUser.username }, [m('img.user-pic', { src: utils.getUserImageOrDefault(currentUser) }), m('span.hidden-sm-down', ' ' + currentUser.username)])),
 	]);
