2688df1baafd98826540af1634eb1f3d7314c8ae
Barry Els <barryels@gmail.com>
2017-05-22 13:30:45 +0200

    fixing "Follow null" button label

--- src/ui/components/UserFollowButton.js
+++ src/ui/components/UserFollowButton.js
@@ -9,11 +9,12 @@ var domain = require('./../../domain');
 
 function view(vnode) {
 	var action = vnode.attrs.action || domain.actions.followUser.bind(null, vnode.attrs.username);
+	var label = vnode.attrs.username ? ' Follow ' + vnode.attrs.username : '';
 
 	return [
 		m('span',
 			m('button.btn.btn-sm.btn-secondary', { onclick: function () { action(); } }, [
-				m('i.ion-plus-round'), m('span', ' Follow ' + vnode.attrs.username)
+				m('i.ion-plus-round'), m('span', label)
 			])
 		)
 	];
--- src/ui/components/UserUnfollowButton.js
+++ src/ui/components/UserUnfollowButton.js
@@ -9,11 +9,12 @@ var domain = require('./../../domain');
 
 function view(vnode) {
 	var action = domain.actions.unfollowUser.bind(null, vnode.attrs.username);
+	var label = vnode.attrs.username ? ' Unfollow ' + vnode.attrs.username : '';
 
 	return [
 		m('span',
 			m('button.btn.btn-sm.btn-secondary', { onclick: function () { action(); } }, [
-				m('i.ion-minus-round'), m('span', ' Unfollow ' + vnode.attrs.username)
+				m('i.ion-minus-round'), m('span', label)
 			])
 		)
 	];
